66f8a6c41a10498df2704ec6b12581ed2236d9f3
Patrick <48617826+GitLet101@users.noreply.github.com>
2021-11-03 14:18:57 -0600

    Add project files.

---
+++ aplicacionKermesse/Controllers/denominacionController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aplicacionKermesse.Models;
+using System.Data.Entity;
+
+namespace aplicacionKermesse.Controllers
+{
+    public class denominacionController : Controller
+    {
+        private BD_KermesseEntities db = new BD_KermesseEntities();
+        // GET: denominacion
+        public ActionResult denominacion()
+        {
+            return View(db.tbl_denominacion.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult FiltrarDenominacion(String cadena)
+        {
+            if (String.IsNullOrEmpty(cadena))
+            {
+                var list = db.tbl_denominacion.ToList();
+                return View("denominacion", list);
+            }
+            else
+            {
+                var listaFiltrada = db.tbl_denominacion.Where(x => x.valor_letras.Contains(cadena));
+                return View("denominacion", listaFiltrada);
+            }
+        }
+
+        public ActionResult guardarDenominacion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(tbl_denominacion tbD)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    tbl_denominacion tbDenom = new tbl_denominacion();
+                    tbDenom.id_moneda = tbD.id_moneda;
+                    tbDenom.valor = tbD.valor;
+                    tbDenom.valor_letras = tbD.valor_letras;
+                    tbDenom.estado = 1;
+                    db.tbl_denominacion.Add(tbDenom);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            var list = db.tbl_denominacion.ToList();
+            return View("denominacion", list);
+        }
+
+        public ActionResult borrarDenominacion(int id)
+        {
+            tbl_denominacion Tdenom = new tbl_denominacion();
+            Tdenom = db.tbl_denominacion.Find(id);
+            db.tbl_denominacion.Remove(Tdenom);
+
+            db.SaveChanges();
+
+            var list = db.tbl_denominacion.ToList();
+            return View("denominacion", list);
+        }
+
+        public ActionResult VerDenominacion(int id)
+        {
+            var denom = db.tbl_denominacion.Where(x => x.id_denominacion == id).First();
+            return View(denom);
+        }
+
+        public ActionResult EditarDenominacion(int id)
+        {
+            tbl_denominacion tDenom = db.tbl_denominacion.Find(id);
+            if (tDenom == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(tDenom);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarDenominacion(tbl_denominacion tDenom)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    tDenom.estado = 2;
+                    db.Entry(tDenom).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                return RedirectToAction("denominacion");
+            }
+            catch (Exception e)
+            {
+                throw e;
+                return View();
+            }
+        }
+    }
+}
---
+++ aplicacionKermesse/Controllers/monedaController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aplicacionKermesse.Models;
+using System.Data.Entity;
+
+namespace aplicacionKermesse.Controllers
+{
+    public class monedaController : Controller
+    {
+        private BD_KermesseEntities db = new BD_KermesseEntities();
+        // GET: moneda
+        public ActionResult moneda()
+        {
+            return View(db.tbl_moneda.ToList());
+        }
+
+        public ActionResult guardarMoneda()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(tbl_moneda tcM)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    tbl_moneda tcMoneda = new tbl_moneda();
+                    tcMoneda.nombre = tcM.nombre;
+                    tcMoneda.signo = tcM.signo;
+                    tcMoneda.estado = 1;
+                    db.tbl_moneda.Add(tcMoneda);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            var list = db.tbl_moneda.ToList();
+            return View("moneda", list);
+        }
+
+        public ActionResult borrarMoneda(int id)
+        {
+            tbl_moneda Tmon = new tbl_moneda();
+            Tmon = db.tbl_moneda.Find(id);
+            db.tbl_moneda.Remove(Tmon);
+
+            db.SaveChanges();
+
+            var list = db.tbl_moneda.ToList();
+            return View("moneda", list);
+        }
+
+        public ActionResult VerMoneda(int id)
+        {
+            var mon = db.tbl_moneda.Where(x => x.id_moneda == id).First();
+            return View(mon);
+        }
+
+        public ActionResult EditarMoneda(int id)
+        {
+            tbl_moneda tMon = db.tbl_moneda.Find(id);
+            if(tMon == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(tMon);
+            }
+        }
+        [HttpPost]
+        public ActionResult ActualizarMoneda(tbl_moneda tMon)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    tMon.estado = 2;
+                    db.Entry(tMon).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                return RedirectToAction("moneda");
+            }
+            catch (Exception e)
+            {
+                throw e;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult FiltrarMoneda(String cadena)
+        {
+            if (String.IsNullOrEmpty(cadena))
+            {
+                var list = db.tbl_moneda.ToList();
+                return View("moneda", list);
+            }
+            else
+            {
+                var listaFiltrada = db.tbl_moneda.Where(x => x.nombre.Contains(cadena) || x.signo.Contains(cadena));
+                return View("moneda", listaFiltrada);
+            }
+        }
+    }
+}
